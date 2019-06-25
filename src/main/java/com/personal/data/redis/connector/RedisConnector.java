aa7497e54944c8de4ffec9f60e35f1d6d4b790d8
xiaokai <zhaijiayu@huli.com>
2019-06-25 09:11:50 +0800

    init

---
+++ src/main/java/com/personal/data/redis/connector/RedisConnector.java
@@ -0,0 +1,9 @@
+package com.personal.data.redis.connector;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+public class RedisConnector extends RedisTemplate {
+
+
+}
